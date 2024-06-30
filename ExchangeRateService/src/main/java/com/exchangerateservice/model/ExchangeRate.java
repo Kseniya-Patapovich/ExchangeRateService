704aaab144fbf9e0158e2d77c56f637ea837e044
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-06-30 09:38:32 +0300

    First commit "Created exchange-rate model"

---
+++ ExchangeRateService/src/main/java/com/exchangerateservice/model/ExchangeRate.java
@@ -0,0 +1,31 @@
+package com.exchangerateservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity(name = "exchange-rate")
+public class ExchangeRate {
+    @Id
+    @SequenceGenerator(name = "rateSeqGen", sequenceName = "exchange_rate_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "rateSeqGen")
+    private Long id;
+
+    @Column(name = "currency_code")
+    private String currencyCode;
+
+    @Column(name = "date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate date;
+
+    @Column(name = "rate")
+    private Double rate;
+}
---
+++ ExchangeRateService/src/main/java/com/exchangerateservice/model/dto/ExchangeRateResponse.java
@@ -0,0 +1,4 @@
+package com.exchangerateservice.model.dto;
+
+public class ExchangeRateResponse {
+}
