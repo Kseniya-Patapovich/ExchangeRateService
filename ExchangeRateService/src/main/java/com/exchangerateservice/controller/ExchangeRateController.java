048ca6c8f468e3203ff4d2c52e5f96d8fd6cc9e1
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-07-01 07:25:57 +0300

    6th commit "Fixed controller"

--- ExchangeRateService/src/main/java/com/exchangerateservice/controller/ExchangeRateController.java
+++ ExchangeRateService/src/main/java/com/exchangerateservice/controller/ExchangeRateController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,18 +22,16 @@ public class ExchangeRateController {
     private final ExchangeRateService exchangeRateService;
 
     @GetMapping("/load")
-    public String loadExchangeRates(@RequestParam("date") LocalDate date) {
-        try {
-            Integer rates = exchangeRateService.fetchAndSaveExchangeRates(date);
-            return "Data loaded successfully: " + rates + " rates";
-        } catch (Exception e) {
-            //return HttpStatus.REQUEST_TIMEOUT + "Error loading data: " + e;
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Error loading data: ", e);
+    public String loadExchangeRates(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        if (date.isAfter(LocalDate.now())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Date cannot be in the future");
         }
+        Integer rates = exchangeRateService.fetchAndSaveExchangeRates(date);
+        return "Data loaded successfully: " + rates + " rates";
     }
 
     @GetMapping
-    public ResponseEntity<ExchangeRate> getExchangeRate(@RequestParam("currencyCode") String currencyCode, @RequestParam("date") LocalDate date) {
+    public ResponseEntity<ExchangeRate> getExchangeRate(@RequestParam("currencyCode") String currencyCode, @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         Optional<ExchangeRate> exchangeRate = exchangeRateService.getExchangeRateByDateAndCurrency(date, currencyCode);
         return exchangeRate.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
